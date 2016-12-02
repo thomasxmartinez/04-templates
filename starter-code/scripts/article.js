faedf307d745f9bd126fcb397e6359a3766e8601
tommytnez <tommytnez@yahoo.com>
2016-12-01 16:11:49 -0800

    this should work now

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -21,6 +21,9 @@ Article.prototype.toHtml = function() {
   //       - Select your template from the DOM.
   //       - Now "compile" your template with Handlebars.
   //       - Don't forget to return your template for this article.
+  var $source = $('#article-template').html();
+  var templateRender = Handlebars.compile($source);
+  return templateRender(this);
 };
 
 ourLocalData.sort(function(a,b) {
